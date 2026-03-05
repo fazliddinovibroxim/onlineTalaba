87d3d1995bea186abf8dd488a4340ad31b9df002
Ibrohim Fazliddinov <fazliddinovibroxim@gmail.com>
2026-03-05 15:51:53 +0500

    Initial commit

---
+++ src/main/java/com/example/onlinetalaba/OnlineTalabaApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinetalaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTalabaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTalabaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onlinetalaba/OnlineTalabaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinetalaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTalabaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
