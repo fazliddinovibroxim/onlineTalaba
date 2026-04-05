dc34519a449ad762212f35f0486e46fd1a5bad98
Ibrohim Fazliddinov <fazliddinovibroxim@gmail.com>
2026-04-05 12:34:12 +0500

    updated

--- src/main/java/com/example/onlinetalaba/config/SecurityConfig.java
+++ src/main/java/com/example/onlinetalaba/config/SecurityConfig.java
@@ -63,8 +63,8 @@ public class SecurityConfig {
                 "http://localhost:8080",
                 "http://localhost:5173",
                 "http://localhost:3000",
-                "https://pulhisob.com",
-                "https://www.pulhisob.com"
+                "https://talaba.pulhisob.com",
+                "https://www.talaba.pulhisob.com"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
